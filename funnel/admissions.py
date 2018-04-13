1d1cceeeb62528ce1de73fe45e4fb6cba64d61ba
Jeff Walton <jwalton@paulsmiths.edu>
2018-04-13 16:36:28 -0400

    admissions processing flow

--- funnel/admissions.py
+++ funnel/admissions.py
@@ -23,10 +23,8 @@ stg_hist = pd.read_csv('STAGEHISTORY.csv', dtype=stg_hist_dtype,
 # create new fields
 stg_hist['year_term'] = (stg_hist['ACADEMIC_YEAR'] + '.' +
                          stg_hist['ACADEMIC_TERM'].str.title())
-# create Revision datetime
-stg_hist['Revision'] = stg_hist.apply(lambda r: pd.datetime.combine(
-                                      r['REVISION_DATE'].date(),
-                                      r['REVISION_TIME'].time()), 1)
+# creation datetime
+stg_hist['create_date'] = stg_hist['FIELD_DATE']
 # convert ACADEMIC_YEAR to numeric keep numeric-valued records
 stg_hist['ACADEMIC_YEAR'] = pd.to_numeric(stg_hist['ACADEMIC_YEAR'],
                                           errors='coerce', downcast='integer')
@@ -38,7 +36,7 @@ stg_hist = stg_hist.loc[pd.to_numeric(stg_hist['ACADEMIC_YEAR'],
 stg_hist.drop(['FIELD_DATE', 'REVISION_DATE', 'REVISION_TIME'],
               inplace=True, axis=1)
 # calculate new fields
-stg_hist['Week_Number'] = stg_hist['Revision'].dt.week
+stg_hist['Week_Number'] = stg_hist['create_date'].dt.week
 stg_hist['Admissions_Week'] = stg_hist.apply(lambda r: (r['Week_Number'] -
                                                         (date(int(
                                                          r['ACADEMIC_YEAR']),
@@ -74,9 +72,46 @@ adm_stat = pd.DataFrame(list(admission_status.items()),
 
 sd1 = (pd.merge(sd1, adm_stat, on=['field_value'], how='left')
        .drop(['field_value', 'status'], axis=1)
-       .drop_duplicates(['PEOPLE_CODE_ID', 'Year_Term', 'Admissions_Week',
+       .drop_duplicates(['PEOPLE_CODE_ID', 'year_term', 'Admissions_Week',
                          'admission_status'])
        .reset_index()
-       .set_index(['Year_Term', 'PEOPLE_CODE_ID', 'admission_status'])
+       .set_index(['year_term', 'PEOPLE_CODE_ID', 'admission_status'])
        .drop(['index'], axis=1)
        )
+
+# Academic Data
+academic_dtype = {'PEOPLE_CODE_ID': str, 'ACADEMIC_YEAR': str,
+                  'ACADEMIC_TERM': str, 'ACADEMIC_SESSION': str,
+                  'APPLICATION_FLAG': str, 'APP_STATUS': str}
+date_cols = ['APPLICATION_DATE', 'APP_STATUS_DATE', 'APP_DECISION_DATE',
+             'REVISION_DATE', 'REVISION_TIME']
+academic = pd.read_csv('ACADEMIC.csv', dtype=academic_dtype,
+                       parse_dates=date_cols,
+                       usecols=['PEOPLE_CODE_ID',
+                                'ACADEMIC_YEAR', 'ACADEMIC_TERM',
+                                'ACADEMIC_SESSION', 'POPULATION',
+                                'INQUIRY_FLAG',
+                                'APPLICATION_FLAG', 'APPLICATION_DATE',
+                                'APP_STATUS', 'APP_STATUS_DATE',
+                                'APP_DECISION', 'APP_DECISION_DATE',
+                                'REVISION_DATE', 'REVISION_TIME'])
+
+app_data = (academic.loc[~(academic['POPULATION'].isin(['ADVSTU', 'NOND'])) &
+                         ((academic['INQUIRY_FLAG'] == 'Y') |
+                          (academic['APPLICATION_FLAG'] == 'Y'))]
+            )
+
+applied = (app_data[app_data['APP_STATUS'].notnull()]
+           .rename(columns={'APP_STATUS': 'field_value'})
+           .rename(columns={'APP_STATUS_DATE': 'Revision'})
+           )
+applied.loc[:, 'field_name'] = 'Application Status'
+
+accepted = (app_data[app_data['APP_DECISION'].notnull()]
+            .rename(columns={'APP_DECISION': 'field_value'})
+            .rename(columns={'APP_DECISION_DATE': 'Revision'})
+            )
+accepted.loc[:, 'field_name'] = 'Application Decision'
+
+# stack Stage History, Academic Applied and Academic Accepted
+adm_df = applied.append(accepted)
