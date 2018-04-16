bf03f31476ac250cc00bce2b403501c9d23df97d
Jeff Walton <jwalton@paulsmiths.edu>
2018-04-16 16:10:57 -0400

    admissions processing flow - refactor admissions.py

--- funnel/admissions.py
+++ funnel/admissions.py
@@ -13,78 +13,24 @@ stgrnk.drop(['code_table', 'MEDIUM_DESC',
 stg_hist_dtype = {'PEOPLE_CODE_ID': str, 'ACADEMIC_YEAR': str,
                   'ACADEMIC_TERM': str, 'ACADEMIC_SESSION': str,
                   'FIELD_ID': np.int64}
-date_cols = ['FIELD_DATE', 'REVISION_DATE', 'REVISION_TIME']
+date_cols = ['FIELD_DATE']
 stg_hist = pd.read_csv('STAGEHISTORY.csv', dtype=stg_hist_dtype,
                        parse_dates=date_cols,
                        usecols=['PEOPLE_CODE_ID', 'ACADEMIC_YEAR',
                                 'ACADEMIC_TERM', 'ACADEMIC_SESSION',
-                                'FIELD_ID', 'FIELD_DATE', 'REVISION_DATE',
-                                'REVISION_TIME'])
-# create new fields
-stg_hist['year_term'] = (stg_hist['ACADEMIC_YEAR'] + '.' +
-                         stg_hist['ACADEMIC_TERM'].str.title())
-# creation datetime
+                                'FIELD_ID', 'FIELD_DATE'])
 stg_hist['create_date'] = stg_hist['FIELD_DATE']
-# convert ACADEMIC_YEAR to numeric keep numeric-valued records
-stg_hist['ACADEMIC_YEAR'] = pd.to_numeric(stg_hist['ACADEMIC_YEAR'],
-                                          errors='coerce', downcast='integer')
-stg_hist = stg_hist.loc[pd.to_numeric(stg_hist['ACADEMIC_YEAR'],
-                                      errors='coerce',
-                                      downcast='integer'
-                                      ).notnull()]
-# drop unused fields
-stg_hist.drop(['FIELD_DATE', 'REVISION_DATE', 'REVISION_TIME'],
-              inplace=True, axis=1)
-# calculate new fields
-stg_hist['Week_Number'] = stg_hist['create_date'].dt.week
-stg_hist['Admissions_Week'] = stg_hist.apply(lambda r: (r['Week_Number'] -
-                                                        (date(int(
-                                                         r['ACADEMIC_YEAR']),
-                                                            9, 1
-                                                        ).isocalendar()[1])
-                                                        )
-                                             if (r['Week_Number'] >
-                                                 (date(int(r['ACADEMIC_YEAR']),
-                                                       9, 1).isocalendar()[1]))
-                                             else (53 + r['Week_Number'] -
-                                                   (date(int(
-                                                       r['ACADEMIC_YEAR']),
-                                                    9, 1).isocalendar()[1])),
-                                             axis=1)
-
 stage_data = pd.merge(stg_hist, stgrnk, left_on=['FIELD_ID'],
                       right_on=['STAGERANKING_ID'], how='left')
-
-ad_keep_values = ['300', 'ACC', 'ACXL', 'CANC', 'DEF', 'DEFR', 'DENY', 'DPAC',
-                  'TRDP', 'TRPD', 'TRNS', 'WAIT']
-ad_keep_cols = ['PEOPLE_CODE_ID', 'year_term', 'Admissions_Week',
-                'field_value', 'status']
-sd1 = stage_data.loc[(stage_data['field_value'].isin(ad_keep_values))]
-sd1 = sd1[ad_keep_cols]
-
-admission_status = {'300': 'Applied', 'ACC': 'Accepted', 'ACXL': 'Canceled',
-                    'CANC': 'Canceled', 'DEF': 'Canceled', 'DEFR': 'Canceled',
-                    'DENY': 'Canceled', 'DPAC': 'Deposited',
-                    'TRDP': 'Deposited', 'TRPD': 'Deposited',
-                    'TRNS': 'Accepted', 'WAIT': 'Accepted'}
-adm_stat = pd.DataFrame(list(admission_status.items()),
-                        columns=['field_value', 'admission_status'])
-
-sd1 = (pd.merge(sd1, adm_stat, on=['field_value'], how='left')
-       .drop(['field_value', 'status'], axis=1)
-       .drop_duplicates(['PEOPLE_CODE_ID', 'year_term', 'Admissions_Week',
-                         'admission_status'])
-       .reset_index()
-       .set_index(['year_term', 'PEOPLE_CODE_ID', 'admission_status'])
-       .drop(['index'], axis=1)
-       )
+keep_fields = ['PEOPLE_CODE_ID', 'ACADEMIC_YEAR', 'ACADEMIC_TERM',
+               'ACADEMIC_SESSION', 'field_name', 'field_value', 'create_date']
+stage_data = stage_data.loc[~stage_data['create_date'].isnull(), keep_fields]
 
 # Academic Data
 academic_dtype = {'PEOPLE_CODE_ID': str, 'ACADEMIC_YEAR': str,
                   'ACADEMIC_TERM': str, 'ACADEMIC_SESSION': str,
                   'APPLICATION_FLAG': str, 'APP_STATUS': str}
-date_cols = ['APPLICATION_DATE', 'APP_STATUS_DATE', 'APP_DECISION_DATE',
-             'REVISION_DATE', 'REVISION_TIME']
+date_cols = ['APPLICATION_DATE', 'APP_STATUS_DATE', 'APP_DECISION_DATE']
 academic = pd.read_csv('ACADEMIC.csv', dtype=academic_dtype,
                        parse_dates=date_cols,
                        usecols=['PEOPLE_CODE_ID',
@@ -93,8 +39,7 @@ academic = pd.read_csv('ACADEMIC.csv', dtype=academic_dtype,
                                 'INQUIRY_FLAG',
                                 'APPLICATION_FLAG', 'APPLICATION_DATE',
                                 'APP_STATUS', 'APP_STATUS_DATE',
-                                'APP_DECISION', 'APP_DECISION_DATE',
-                                'REVISION_DATE', 'REVISION_TIME'])
+                                'APP_DECISION', 'APP_DECISION_DATE'])
 
 app_data = (academic.loc[~(academic['POPULATION'].isin(['ADVSTU', 'NOND'])) &
                          ((academic['INQUIRY_FLAG'] == 'Y') |
@@ -103,15 +48,73 @@ app_data = (academic.loc[~(academic['POPULATION'].isin(['ADVSTU', 'NOND'])) &
 
 applied = (app_data[app_data['APP_STATUS'].notnull()]
            .rename(columns={'APP_STATUS': 'field_value'})
-           .rename(columns={'APP_STATUS_DATE': 'Revision'})
+           .rename(columns={'APP_STATUS_DATE': 'create_date'})
            )
 applied.loc[:, 'field_name'] = 'Application Status'
+applied = applied.loc[~applied['create_date'].isnull(), keep_fields]
 
 accepted = (app_data[app_data['APP_DECISION'].notnull()]
             .rename(columns={'APP_DECISION': 'field_value'})
-            .rename(columns={'APP_DECISION_DATE': 'Revision'})
+            .rename(columns={'APP_DECISION_DATE': 'create_date'})
             )
 accepted.loc[:, 'field_name'] = 'Application Decision'
+accepted = accepted.loc[~accepted['create_date'].isnull(), keep_fields]
 
 # stack Stage History, Academic Applied and Academic Accepted
-adm_df = applied.append(accepted)
+adm_df = stage_data.append(applied).append(accepted)
+
+adm_df = (adm_df.loc[(adm_df['ACADEMIC_TERM'].isin(['FALL', 'SPRING'])) &
+                     (adm_df['ACADEMIC_SESSION'] == 'MAIN')]
+          )
+
+# create new fields
+adm_df['year_term'] = (adm_df['ACADEMIC_YEAR'] + '.' +
+                       adm_df['ACADEMIC_TERM'].str.title())
+adm_df['Week_Number'] = adm_df['create_date'].dt.week
+
+# convert ACADEMIC_YEAR to numeric keep numeric-valued records
+adm_df['ACADEMIC_YEAR'] = pd.to_numeric(adm_df['ACADEMIC_YEAR'],
+                                        errors='coerce', downcast='integer')
+adm_df = adm_df.loc[adm_df['ACADEMIC_YEAR'].notnull()]
+
+adm_week_number = (lambda r: (r['Week_Number'] -
+                              (date(int(r['ACADEMIC_YEAR']), 9, 1)
+                               .isocalendar()[1])
+                              )
+                   if (r['Week_Number'] > (date(int(r['ACADEMIC_YEAR']), 9, 1)
+                                           .isocalendar()[1]))
+                   else (53 + r['Week_Number'] -
+                         (date(int(r['ACADEMIC_YEAR']), 9, 1)
+                          .isocalendar()[1])
+                         )
+                   )
+adm_df['Admissions_Week'] = adm_df.apply(adm_week_number, axis=1)
+
+adm_keep_values = ['300', 'ACC', 'ACXL', 'CANC', 'DEF', 'DEFR', 'DENY', 'DPAC',
+                   'TRDP', 'TRPD', 'TRNS', 'WAIT']
+adm_keep_cols = ['PEOPLE_CODE_ID', 'year_term', 'Admissions_Week',
+                 'field_value']
+adm_df = adm_df.loc[(adm_df['field_value'].isin(adm_keep_values)),
+                    adm_keep_cols]
+
+# admissions status table
+admission_status = {'300': 'Applied', 'ACC': 'Accepted', 'ACXL': 'Canceled',
+                    'CANC': 'Canceled', 'DEF': 'Canceled', 'DEFR': 'Canceled',
+                    'DENY': 'Canceled', 'DPAC': 'Deposited',
+                    'TRDP': 'Deposited', 'TRPD': 'Deposited',
+                    'TRNS': 'Accepted', 'WAIT': 'Accepted'}
+adm_stat = pd.DataFrame(list(admission_status.items()),
+                        columns=['field_value', 'admission_status'])
+
+adm_df1 = (pd.merge(adm_df, adm_stat, on=['field_value'], how='left')
+           .drop(['field_value'], axis=1)
+           .drop_duplicates(['PEOPLE_CODE_ID', 'year_term', 'Admissions_Week',
+                             'admission_status'])
+           )
+
+adm_df1 = (adm_df1.sort_values(['year_term', 'PEOPLE_CODE_ID',
+                                'Admissions_Week'])
+           .drop_duplicates(['year_term', 'PEOPLE_CODE_ID',
+                             'admission_status'],
+                            keep='first')
+           )
