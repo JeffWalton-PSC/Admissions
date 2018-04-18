247400a4c4997aa696132be8ae3f03795fab19cc
Jeff Walton <jwalton@paulsmiths.edu>
2018-04-18 14:20:38 -0400

    admissions processing flow - add db connection

--- funnel/admissions.py
+++ funnel/admissions.py
@@ -2,23 +2,33 @@ import pandas as pd
 import numpy as np
 from datetime import datetime, date
 
+import os
+from sqlalchemy import create_engine
+# local connection information
+db_user = os.environ.get('DB_USER')
+db_pass = os.environ.get('DB_PASS')
+engine = create_engine(f'mssql+pyodbc://{db_user}:{db_pass}' +
+                       '@PSC-SQLProd/Campus6?' +
+                       'driver=ODBC+Driver+13+for+SQL+Server')
+connection = engine.connect()
 
-# read VWSTAGERANKING.csv and drop unused fields
-stgrnk = pd.read_csv('VWSTAGERANKING.csv')
-stgrnk.drop(['code_table', 'MEDIUM_DESC',
-             'Converted/Confirmed/Accepted/Require SepDate'],
-            inplace=True, axis=1)
+today = datetime.now().strftime('%Y%m%d')
 
-# read STAGEHISTORY.csv
-stg_hist_dtype = {'PEOPLE_CODE_ID': str, 'ACADEMIC_YEAR': str,
-                  'ACADEMIC_TERM': str, 'ACADEMIC_SESSION': str,
-                  'FIELD_ID': np.int64}
-date_cols = ['FIELD_DATE']
-stg_hist = pd.read_csv('STAGEHISTORY.csv', dtype=stg_hist_dtype,
-                       parse_dates=date_cols,
-                       usecols=['PEOPLE_CODE_ID', 'ACADEMIC_YEAR',
-                                'ACADEMIC_TERM', 'ACADEMIC_SESSION',
-                                'FIELD_ID', 'FIELD_DATE'])
+begin_year = '2009'
+
+# read VWSSTAGERANKING data
+sql_str = "SELECT STAGERANKING_ID, field_name, field_value " + \
+          "FROM VWSSTAGERANKING WHERE " + \
+          "status = 'A' "
+stgrnk = pd.read_sql_query(sql_str, connection)
+
+# read STAGEHISTORY data
+sql_str = "SELECT PEOPLE_CODE_ID, ACADEMIC_YEAR, ACADEMIC_TERM, " + \
+          "ACADEMIC_SESSION, FIELD_ID, FIELD_DATE " + \
+          "FROM STAGEHISTORY WHERE " + \
+          f"ACADEMIC_YEAR >= '{begin_year}' "
+stg_hist = pd.read_sql_query(sql_str, connection)
+
 stg_hist['create_date'] = stg_hist['FIELD_DATE']
 stage_data = pd.merge(stg_hist, stgrnk, left_on=['FIELD_ID'],
                       right_on=['STAGERANKING_ID'], how='left')
@@ -26,7 +36,7 @@ keep_fields = ['PEOPLE_CODE_ID', 'ACADEMIC_YEAR', 'ACADEMIC_TERM',
                'ACADEMIC_SESSION', 'field_name', 'field_value', 'create_date']
 stage_data = stage_data.loc[~stage_data['create_date'].isnull(), keep_fields]
 
-# Academic Data
+# read ACADEMIC data
 academic_dtype = {'PEOPLE_CODE_ID': str, 'ACADEMIC_YEAR': str,
                   'ACADEMIC_TERM': str, 'ACADEMIC_SESSION': str,
                   'APPLICATION_FLAG': str, 'APP_STATUS': str}
@@ -94,7 +104,7 @@ adm_week_number = (lambda r: (r['Week_Number'] -
 adm_df['Admissions_Week'] = adm_df.apply(adm_week_number, axis=1)
 
 adm_keep_values = ['300', 'ACC', 'ACXL', 'CANC', 'DEF', 'DEFR', 'DENY', 'DPAC',
-                   'TRDP', 'TRPD', 'TRNS', 'WAIT']
+                   'TRDP', 'TRPD', 'TRNS', 'WAIT', '500', 'PEND', 'COMP']
 adm_keep_cols = ['PEOPLE_CODE_ID', 'year_term', 'Admissions_Week',
                  'field_value']
 adm_df = adm_df.loc[(adm_df['field_value'].isin(adm_keep_values)),
@@ -105,7 +115,10 @@ admission_status = {'300': 'Applied', 'ACC': 'Accepted', 'ACXL': 'Canceled',
                     'CANC': 'Canceled', 'DEF': 'Canceled', 'DEFR': 'Canceled',
                     'DENY': 'Canceled', 'DPAC': 'Deposited',
                     'TRDP': 'Deposited', 'TRPD': 'Deposited',
-                    'TRNS': 'Accepted', 'WAIT': 'Accepted'}
+                    'TRNS': 'Accepted', 'WAIT': 'Accepted',
+                    '500': 'Deposited', 'PEND': 'Applied',
+                    'COMP': 'Applied',
+                    }
 adm_stat = pd.DataFrame(list(admission_status.items()),
                         columns=['field_value', 'admission_status'])
 
@@ -116,7 +129,7 @@ adm_df1 = (pd.merge(adm_df, adm_stat, on=['field_value'], how='left')
            )
 
 adm_df1 = (adm_df1.sort_values(['year_term', 'PEOPLE_CODE_ID',
-                                'Admissions_Week'])
+                                'admission_status', 'Admissions_Week'])
            .drop_duplicates(['year_term', 'PEOPLE_CODE_ID',
                              'admission_status'],
                             keep='first')
