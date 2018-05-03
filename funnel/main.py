793d2e4b4400227fe76257a9b69c4200a72360c2
Jeff Walton <jwalton@paulsmiths.edu>
2018-05-03 15:53:41 -0400

    admissions bokeh plot

--- funnel/main.py
+++ funnel/main.py
@@ -32,13 +32,6 @@ def adm_week(d):
 
 df = pd.read_hdf('funnel/data/stage_data', key='weekly')
 
-all_terms = sorted(list(df['year_term'].dropna().unique()))
-this_term = Select(title="Current Term:", value=all_terms[-1], options=all_terms)
-if this_term in all_terms:
-    all_terms.remove(this_term)
-terms = MultiSelect(title="Display Other Terms:", value=None,
-                           options=[(i,i) for i in all_terms ])
-
 summ = df.groupby(['year_term', 'stage']).sum()
 summ_t = summ.transpose()
 
@@ -46,21 +39,21 @@ week_number, adm_week_number = adm_week(today)
 # curr_list = sorted(list(df['curriculum'].dropna().unique()))
 
 
-def create_figure():
-    stage = stage_list[stage_rg.active]
-    title = f"{stage} - Admissions Weekly Summary - Week {adm_week_number:d} ({today_str})"
+def create_figure(df, title, stage, term):
+    
+    
 
     p = figure(plot_width=800, plot_height=600, title=title,
            x_axis_label="Week Number", y_axis_label=stage,
            tools="pan,wheel_zoom,box_zoom,save,reset",
           )
-    c=5
+    #c=5
     
-    for t in terms:
-        p.line(df.index, summ_t[(t, stage)], color=Blues8[c], legend=t)
-        c -= 1
+#    for t in terms.value:
+#        p.line(df.index, summ_t[(t, stage)], color=Blues8[c], legend=t)
+#        c -= 1
 
-    p.line(summ_t.index, summ_t[(this_term, stage)], color='red', legend=this_term)
+    p.line(df.index, df[(term, stage)], color='red', legend=term)
     # week_number line
     # p.line((adm_week_number,adm_week_number),
     #       (-1000,5000), color='green', legend=None)
@@ -71,15 +64,32 @@ def create_figure():
 
 
 def update(attr, old, new):
-    layout.children[1] = create_figure()
+    layout.children[1] = create_figure(summ_t, title, stage, current_term.value)
 
 
 stage_list = ['Applied', 'Accepted', 'Deposited']
-stage_rg = RadioGroup(labels=stage_list, active=2)
+stage_rg = RadioGroup(name='Stage:', labels=stage_list, active=2)
 stage_rg.on_change('active', update)
+stage = stage_list[stage_rg.active]
 
-controls = widgetbox([stage_rg])
-layout = row(controls, create_figure())
+title = f"{stage} - Admissions Weekly Summary - Week {adm_week_number:d} ({today_str})"
+
+all_terms = sorted(list(df['year_term'].dropna().unique()))
+current_term = Select(title="Current Term:", value=all_terms[-2], options=all_terms)
+
+other_terms = all_terms.copy()
+if current_term.value in other_terms:
+    other_terms.remove(current_term.value)
+terms_opt = [(i,i) for i in other_terms ]
+print('terms_opt', terms_opt)
+# terms = MultiSelect(title="Display Other Terms:", value=None,
+#                     options=terms_opt)
+
+
+controls = widgetbox([stage_rg, current_term])
+##print('controls', controls)
+layout = row(controls, create_figure(summ_t, title, stage, current_term.value))
+##print ('layout', layout)
 
 curdoc().add_root(layout)
 curdoc().title = "Admissions Weekly Report"
