642ea3ebd6fb1a2788c0c1b7e9611ef9f459d866
Jeff Walton <jwalton@paulsmiths.edu>
2026-04-06 11:48:05 -0400

    edit Admissions_funnel_data.bat

--- funnel/Admissions_funnel_data.bat
+++ funnel/Admissions_funnel_data.bat
@@ -1,23 +1,23 @@
 @echo on
-echo "***** batch file running" >> F:\Applications\Admissions\funnel\logs\out.log
-date /T >> F:\Applications\Admissions\funnel\logs\out.log
-time /T >> F:\Applications\Admissions\funnel\logs\out.log
+echo "***** batch file running" >> F:\Applications\Admissions\funnel\logs\out_stage_data.log
+date /T >> F:\Applications\Admissions\funnel\logs\out_stage_data.log
+time /T >> F:\Applications\Admissions\funnel\logs\out_stage_data.log
 
-whoami 1>> F:\Applications\Admissions\funnel\logs\out.log 2>&1
-echo "whoami" 1>> F:\Applications\Admissions\funnel\logs\out.log 2>&1
+whoami 1>> F:\Applications\Admissions\funnel\logs\out_stage_data.log 2>&1
+echo "whoami" 1>> F:\Applications\Admissions\funnel\logs\out_stage_data.log 2>&1
 
 F:
-echo "F:" 1>> F:\Applications\Admissions\funnel\logs\out.log 2>&1
+echo "F:" 1>> F:\Applications\Admissions\funnel\logs\out_stage_data.log 2>&1
 cd F:\Applications\Admissions\funnel
-echo "cd" 1>> F:\Applications\Admissions\funnel\logs\out.log 2>&1
+echo "cd" 1>> F:\Applications\Admissions\funnel\logs\out_stage_data.log 2>&1
 
-call C:\ProgramData\Anaconda3\condabin\activate.bat py311streamlit 1>> F:\Applications\Admissions\funnel\logs\out.log 2>&1
-echo "call activate" 1>> F:\Applications\Admissions\funnel\logs\out.log 2>&1
-rem call conda list 1>> F:\Applications\Admissions\funnel\logs\out.log 2>&1
+call C:\ProgramData\Anaconda3\condabin\activate.bat py311streamlit 1>> F:\Applications\Admissions\funnel\logs\out_stage_data.log 2>&1
+echo "call activate" 1>> F:\Applications\Admissions\funnel\logs\out_stage_data.log 2>&1
+rem call conda list 1>> F:\Applications\Admissions\funnel\logs\out_stage_data.log 2>&1
 
-echo "START Admissions\funnel\admissions.py " >> F:\Applications\Admissions\funnel\logs\out.log
-C:\ProgramData\Anaconda3\envs\py311streamlit\python.exe admissions.py 1>> F:\Applications\Admissions\funnel\logs\out.log 2>&1
+echo "START Admissions\funnel\admissions.py " >> F:\Applications\Admissions\funnel\logs\out_stage_data.log
+C:\ProgramData\Anaconda3\envs\py311streamlit\python.exe admissions.py 1>> F:\Applications\Admissions\funnel\logs\out_stage_data.log 2>&1
 
 rem pause
-time /T >> F:\Applications\Admissions\funnel\logs\out.log
-echo "***** batch file exiting" >> F:\Applications\Admissions\funnel\logs\out.log
+time /T >> F:\Applications\Admissions\funnel\logs\out_stage_data.log
+echo "***** batch file exiting" >> F:\Applications\Admissions\funnel\logs\out_stage_data.log
